23023972ee123eea8be5fee8d636927514460a84
skm_sarah <39684855+skm0626@users.noreply.github.com>
2021-09-19 21:57:06 +0900

    Add files via upload

--- src/delivery_mission.py
+++ src/delivery_mission.py
@@ -18,14 +18,14 @@ slidingwindow = SlidingWindow()
 img = np.empty(shape=[0])
 
 pixel = 80.0/200.0 # 0.4cm
-invisible_distance = 366
+invisible_distance = 430#366
 center = np.array([320,480,1], np.float32)
 
 # black square points
-up_left = [268,297]
-up_right = [365,297]
-down_left = [260,318]
-down_right = [377,318]
+up_left = [223,381]
+up_right = [369,381]
+down_left = [217,422]
+down_right = [407,422]
 corner_points_array = np.float32([up_left,up_right,down_left,down_right])
 
 box_class = None
@@ -40,6 +40,10 @@ matrix = None
 delivery_pub = rospy.Publisher('delivery', DeliveryArray, queue_size=10)
 
 data_list = []
+
+#test visualization
+a = np.array([-1,-1,-1], np.float32)
+b = np.array([-1,-1,-1], np.float32)
 
 def image_callback(img_data):
 	global bridge
@@ -60,8 +64,8 @@ def bounding_callback(msg):
 	# bounding_list = []
 	if np.any(matrix) == None: return
 	
-	data_list = []
-	
+	# data_list = []
+	#print("**********************************************************************")
 	for idx, box in enumerate(bbox):
 		b_flag = -1
 		box_class = box.Class
@@ -71,39 +75,59 @@ def bounding_callback(msg):
 		box_ymax = box.ymax
 
 		# b1(0), b2(1), b3(2)
-		if box_class == "B1": b_flag = 0
-		if box_class == "B2": b_flag = 1
-		if box_class == "B3": b_flag = 2
-	
+		if box_class == "B1": b_flag = 1
+		if box_class == "B2": b_flag = 2
+		if box_class == "B3": b_flag = 3
 
-		if box_class == "A1": b_flag = 3
+		if box_class == "A1": b_flag = 4
+		if box_class == "A2": b_flag = 5
+		if box_class == "A3": b_flag = 6
 
-		tf_object_center = get_object_center2(box.Class,box_xmin, box_ymin, box_xmax, box_ymax)
-		tf_center = np.matmul(matrix, center)
+		tf_object_center = get_object_center2(box.Class,box.xmin, box.ymin, box.xmax, box.ymax)
+		tf_center = np.matmul(matrix, center.T)
 		tf_center /= tf_center[2]
-
+		# print("IMAGE CENTER : ", center)
+		# print("TRANSFOREMD CENTER : ", tf_center)
+		# print("REFINED CENTER : ", tf_center)
+		#print("::::::::::::::center:::::::::::", tf_center)
+		#print("BOX CLASS : ", box.Class)
 		distance = calculate(tf_center, tf_object_center)
+		#print("DISTANCE : ", distance)
 		# print("{}) tf_center: {}, distance: {}".format(idx, tf_center, distance))
-		
+
 		if b_flag != -1:
 			delivery = Delivery()
 			delivery.flag = b_flag
-			delivery.x = distance[0]
-			delivery.y = distance[1]
+			delivery.dist_x = distance[0]
+			delivery.dist_y = distance[1]
 
 			data_list.append(delivery)
-	
+
 	if len(data_list) > 0:
-		data_list = sorted(data_list, key=lambda x:(x[2],x[1],x[0]))
+		# data_list = sorted(data_list, key=lambda x:(x[2],x[1],x[0]))
+		# data_list = sorted(data_list, key=lambda x:(x.dist_y,x.dist_x,x.flag))
 		delivery_array = DeliveryArray()
 		delivery_array.visions = data_list
 		delivery_pub.publish(delivery_array)
-	
+	#print("**********************************************************************")
 
-def get_object_center2(box_class,xmin, ymin, xmax, ymax):  
+	return
+
+
+def get_object_center2(box_class,xmin, ymin, xmax, ymax):
 	object_center = np.array([(xmin + xmax) / 2, ymax, 1], np.float32)
-	tf_object_center = np.matmul(matrix, object_center)
-	tf_object_center /= tf_object_center[2]
+	# print("* ORIGINAL OBJECT CENTER : {} {}".format(box_class, object_center))
+	tf_object_center = np.matmul(matrix, object_center.T)
+	# print("matrix.shape : ", matrix.shape)
+	# print("object_center.shape : ", object_center.shape)
+	# print("* RAW TRANSFORMED OBJECT CENTER : ", tf_object_center)
+	# print("object__center:::::::::::", tf_object_center)
+	if tf_object_center[2] > 0:
+		tf_object_center /= -tf_object_center[2]
+	else:
+		tf_object_center /= tf_object_center[2]
+	# print("* REFINE TRANSFORMED OBJECT CENTER : ", tf_object_center)
+	
 
 	#if box_class == "B1":
 		#print("B1 : ", tf_object_center)
@@ -115,12 +139,22 @@ def get_object_center2(box_class,xmin, ymin, xmax, ymax):
 	return tf_object_center
 
 def calculate(tf_center, tf_object_center):
+	global a,b
+	a = tf_center
+	b = tf_object_center
+
+	# calc distance
 	distance = tf_center - tf_object_center
+	#print("TF_CENTER : ", tf_center)
+	#print("TF_OBJECT_CENTER : ", tf_object_center)
+	#print("DISTANCE : ", distance)
+
 	distance = distance * pixel
 	distance[1] += invisible_distance
-	#print("distance", distance)
+
+	#print("distance", distance[1])
 	# print("distance[0]", int(distance[0]), int(distance[1]))
-	return distance	
+	return distance
 
 
 # center_visualization
@@ -129,16 +163,15 @@ def check_center(image):
 	return image
 	
 
-
 if __name__ == '__main__':
 	global matrix
-	#global data_list
+	global data_list
 	#global img
 	rospy.init_node('warp')
 	# Video 용 Subscriber
-	# image_sub = rospy.Subscriber("/videofile/image_raw/", Image, image_callback)
+	image_sub = rospy.Subscriber("/videofile/image_raw/", Image, image_callback)
 	# USB CAMERA 용 Subscriber	
-	image_sub = rospy.Subscriber("/usb_cam/image_raw/", Image, image_callback)
+	#image_sub = rospy.Subscriber("/usb_cam/image_raw/", Image, image_callback)
 	
 	#cap = cv2.VideoCapture("/home/foscar/ISCC_2021/src/vision_distance/src/ISCC_2021_Vision/yesun/8-31/origin_2021-8-31-19-42.avi")
 	bbox_sub = rospy.Subscriber("/darknet_ros/bounding_boxes/", BoundingBoxes, bounding_callback)
@@ -151,7 +184,8 @@ if __name__ == '__main__':
 		#print(ret)
 		if img.size != (640 * 480 * 3):
                     continue
-		
+		data_list = []
+
 		width = 1000
 	    	height = 850
 		
@@ -162,17 +196,17 @@ if __name__ == '__main__':
 		#else:
 		#	continue
 	
-		img_up_left = [400,600]#[450, 650] #[220,150] #[400,600]
-		img_up_right = [600,600]#[550, 650] #[420,150] #[600,600]
-		img_down_left = [400,800]#[450, 750] #[220,350] #[400,800]
-		img_down_right = [600,800]#[550, 750] #[420,350] #[600,800]
+		img_up_left = [400,600] #[450, 650] #[220,150] #[400,600]
+		img_up_right = [600,600] #[550, 650] #[420,150] #[600,600]
+		img_down_left = [400,800] #[450, 750] #[220,350] #[400,800]
+		img_down_right = [600,800] #[550, 750] #[420,350] #[600,800]
 		img_params = np.float32([img_up_left, img_up_right, img_down_left, img_down_right])
 
 	    	# Compute and return the transformation matrix
 	    	matrix = cv2.getPerspectiveTransform(corner_points_array, img_params)
 		np_matrix = np.array(matrix)
 		np.save(matrix_path, np_matrix)
-		# print(np_matrix)
+		#print(np_matrix)
 	    	img_transformed = cv2.warpPerspective(img, matrix, (width, height))
 
 		if box_xmin == None or box_ymin == None or box_xmax == None or box_ymax == None: continue 
@@ -187,12 +221,13 @@ if __name__ == '__main__':
 
 		warp_xymin = np.matmul(np_matrix, warp_xymin)
 		warp_xymax = np.matmul(np_matrix, warp_xymax)
+		
 		warp_xymin /= warp_xymin[2]
 		warp_xymax /= warp_xymax[2]
 
 		img = check_center(img)
 		# print('class name', box_class)
-
+    
 		cv2.circle(img, (up_left[0], up_left[1]), 5, (255,0,0), -1)
 		cv2.circle(img, (up_right[0], up_right[1]), 5, (0,255,0), -1)
 		cv2.circle(img, (down_left[0], down_left[1]), 5, (0,0,255), -1)
@@ -203,8 +238,9 @@ if __name__ == '__main__':
 		cv2.circle(img, (box_xmin, box_ymax), 5, (122,0,0), -1)
 		cv2.circle(img, (box_xmax, box_ymax), 5, (122,0,0), -1)
 
-		cv2.circle(img, (288,480), 5, (255,0,0),-1 ) #center
-
+		cv2.circle(img, (288,480), 5, (255,0,0),-1 ) # center
+		cv2.circle(img_transformed, (int(a[0]),int(a[1])),10,(122,134,115),-1)
+		cv2.circle(img_transformed, (int(b[0]),int(b[1])),10,(0,0,155),-1)
 
 		if (len(new_data_list) > 0):
 			left_point = np.array([70.18,520,1], np.float32) #[184.18,520,1]
@@ -214,10 +250,22 @@ if __name__ == '__main__':
 			warp_right_point = np.matmul(np_matrix, right_point)
 			warp_right_point /= warp_right_point[2]
 
+		# data_list sort
+		'''
+		sorted_data_list = sorted(data_list, key=lambda x:(x.dist_y,x.dist_x))
+		print(len(data_list))
+		if len(sorted_data_list):
+			print("* CLOSEST B FLAG : ", sorted_data_list[0].flag)
+		else:
+			print("* Not Detected ")
+		'''
 
-		#if cv2.waitKey(1) & 0xFF == ord('q'):
-		#	break    		
-		cv2.waitKey(33)
+
+		# if cv2.waitKey(1) & 0xFF == ord('q'):
+		#	break
+		# cv2.imshow("original_img", img)   
+		# cv2.imshow("warp", img_transformed) 		
+		# cv2.waitKey(1)
 		rate.sleep()
 
 	cv2.destroyAllWindows()
